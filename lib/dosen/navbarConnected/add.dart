5e1d312d5544a54073913dcbc227b477cbfe7db9
Christiano Sinaga <christianozetroabsinaga@gmail.com>
2023-10-31 12:57:45 +0700

    Create add.dart

---
+++ lib/dosen/navbarConnected/add.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTicket extends StatefulWidget {
+  const AddTicket({super.key});
+
+  @override
+  State<AddTicket> createState() => _AddTicketState();
+}
+
+class _AddTicketState extends State<AddTicket> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
